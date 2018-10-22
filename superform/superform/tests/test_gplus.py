5a671c3db90d9d4b842c7757aeada5b843cb23b8
Gui <guillaume.gheysen@student.uclouvain.be>
2018-10-22 11:38:55 +0200

    upgrade test

--- superform/superform/tests/test_gplus.py
+++ superform/superform/tests/test_gplus.py
@@ -2,61 +2,40 @@ import datetime
 import os
 import tempfile
 
-import pytest
 import json
 import unittest
 
-import superform.plugins.Gplus as Gplus
-import publishings as Publishing
+import plugins.Gplus as Gplus
 
 
-from superform import app, db
+class Publishing:
+    def __init__(self, title, description, link, image, from_, until_, channel_id = "Google+ INGI-UCL"):
+        self.title = title
+        self.description = description
+        self.link = link
+        self.image = image
+        self.from_ = from_
+        self.until_ = until_
+        self.channel_id = channel_id
+
 
 class TestGPlus(unittest.TestCase):
     def test_login(self):
         pass
 
-    def test_publish(self):
-        my_publy = Publishing()
+    def test_publishing(self):
+        my_publy = Publishing("Why Google+ is still relevant, even though it will soon cease to exist",
+                              "And Jesus said : This is my body",
+                              "www.chretienDeTroie.fr",
+                              None, " 24-12-2018", "12-12-2222")
+        retur = Gplus.create_activity_body(my_publy)
+        self.assertEqual(retur.get('originalContent', 'No content'), my_publy.description, "Problem with content")
+        self.assertEqual(retur.get('url', 'No content'), my_publy.link, "Problem with the link url")
+        self.assertEqual(retur.get('image', 'No content'), my_publy.image, "Problem with the image url")
+        self.assertEqual(retur.get('from', 'No content'), my_publy.from_, "Problem with the from date")
+        self.assertEqual(retur.get('until', 'No content'), my_publy.until_, "Problem with the until date")
+        self.assertNotEquals((retur.get("Title", "No Title")), my_publy.title, "There should be no Title !")
+
 
 if __name__ == "__main__":
     unittest.main()
-
-
-@pytest.fixture
-def client():
-    app.app_context().push()
-    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-    app.config['TESTING'] = True
-    client = app.test_client()
-
-    with app.app_context():
-        db.create_all()
-
-    yield client
-
-    os.close(db_fd)
-    os.unlink(app.config['DATABASE'])
-
-
-def test_run_gplus(client):
-    # Is there a way to test a publishing method ? I think Yes ...
-
-    assert True == True
-
-
-def test_publish(publishing, channel_publishing):
-    my_publi = Gplus.create_activity_body(publishing)
-    assert my_publi is not None
-    assert is_json(my_publi)
-    assert my_publi.get('object').get('originalContent') == publishing.description
-    #assert Gplus.run(publishing, channel_publishing)
-
-
-def is_json(myjson):
-    try:
-        json.loads(myjson)
-    except ValueError:
-        return False
-    return True
-
