d769e93918e5a36bb00c668f4c9288cc8b61d79e
Sami <sami.bosch@gmail.com>
2018-10-23 18:00:19 +0200

    circles slight change

--- superform/superform/posts.py
+++ superform/superform/posts.py
@@ -72,7 +72,7 @@ def new_post():
     #for chan in list_of_channels:
     #    if chan.module=='superform.plugins.Gplus':
     #        circles[chan.id] = list_circle(chan.config)
-    circles[1] = ['My domain', 'phillliiiipe', 'tamere', '69', '42']
+    circles[1] = ['My Domain (Publish in Public)', 'phillliiiipe', 'tamere', '69', '42']
 
     for elem in list_of_channels:
         m = elem.module
