4048d807172430b3467d02fa12d084413bd4a4fe
Gui <guillaume.gheysen@student.uclouvain.be>
2018-10-24 10:57:00 +0200

    upgrading test

--- superform/superform/tests/test_gplus.py
+++ superform/superform/tests/test_gplus.py
@@ -10,7 +10,9 @@ import plugins.Gplus as Gplus
 
 class Publishing:
     def __init__(self, post_id, title, description, link_url, image_url,
-                 date_from, date_until, channel_id="Google+ INGI-UCL", state=-1):
+                 date_from, date_until, option=None, channel_id="Google+ INGI-UCL", state=-1):
+        if option is None:
+            option = [{"disablesharing": True, "disablecomments" : True, "circles": "Fanfan_Le_FDP"}]
         self.post_id = post_id
         self.channel_id = channel_id
         self.state = state
@@ -20,6 +22,7 @@ class Publishing:
         self.image_url = image_url
         self.date_from = date_from
         self.date_until = date_until
+        self.extra = json.dumps(option)
 
 
 class TestGPlus(unittest.TestCase):
@@ -34,12 +37,15 @@ class TestGPlus(unittest.TestCase):
                               "www.chretienDeTroie.fr",
                               None, " 24-12-2018", "12-12-2222")
         retur = Gplus.create_activity_body(my_publy)
-        self.assertEqual(retur.get('originalContent', 'No content'), my_publy.description, "Problem with content")
-        self.assertEqual(retur.get('url', 'No content'), my_publy.link_url, "Problem with the link url")
-        self.assertEqual(retur.get('image', 'No content'), my_publy.image_url, "Problem with the image url")
-        self.assertEqual(retur.get('from', 'No content'), my_publy.date_from, "Problem with the from date")
-        self.assertEqual(retur.get('until', 'No content'), my_publy.date_until, "Problem with the until date")
-        self.assertNotEquals((retur.get("Title", "No Title")), my_publy.title, "There should be no Title !")
+        self.assertEqual(retur['object'].get('originalContent', 'No content'), my_publy.description, "Problem with content")
+        for a in retur['object']['attachements']:
+            if a.get('objectType') == "article":
+                self.assertEqual(retur['object']['attachements'].get('url', 'No content'), my_publy.link_url,
+                                 "Problem with the link url")
+            elif a.get('objectType') == "photo":
+                self.assertEqual(retur['object']['attachements'].get('url', 'No content'), my_publy.image_url,
+                                 "Problem with the image url")
+        self.assertNotEquals((retur['object']['attachements'].get("Title", "No Title")), my_publy.title, "There should be no Title !")
 
 
 if __name__ == "__main__":
